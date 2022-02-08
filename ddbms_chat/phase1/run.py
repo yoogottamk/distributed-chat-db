cabd2a30d2efcfe074f79532908bfbcfa89cde34
yoogottamk <yoogottamk@outlook.com>
2022-02-08 17:45:36 +0530

    restructuring

--- ddbms_chat/phase1/run.py
+++ ddbms_chat/phase1/run.py
@@ -1,12 +1,11 @@
-from ddbms_chat.phase1.db import recreate_db
-from ddbms_chat.phase1.tables import setup_tables, fill_tables
+from ddbms_chat.phase1 import db, syscat_tables
 from ddbms_chat.syscat.sites import SITES
 from ddbms_chat.utils import DBConnection
 
 message_function_map = [
-    ("Creating database", recreate_db, False),
-    ("Creating system catalog tables", setup_tables, True),
-    ("Filling system catalog tables", fill_tables, True),
+    ("Creating database", db.recreate_db, False),
+    ("Creating system catalog tables", syscat_tables.setup_tables, True),
+    ("Filling system catalog tables", syscat_tables.fill_tables, True),
 ]
 
 for message, func, connect_db in message_function_map:
