f953197ea7eac9381a87713ea469832900e852d8
yoogottamk <yoogottamk@outlook.com>
2022-05-06 06:34:06 +0530

    try join for joining tables instead of filtering cartesian product

--- ddbms_chat/phase3/daemon.py
+++ ddbms_chat/phase3/daemon.py
@@ -126,8 +126,8 @@ def exec_query(action: str):
                 join_condition = condition_dict_to_object(join_condition)
                 query = (
                     f"create table `{target_relation_name}` as "
-                    f"select * from `{relation1_name}`, `{relation2_name}` "
-                    f"where {construct_select_condition_string(join_condition, relation1_name, relation2_name)}"
+                    f"select * from `{relation1_name}` join `{relation2_name}` "
+                    f"on {construct_select_condition_string(join_condition, relation1_name, relation2_name)}"
                 )
                 debug_log(query)
                 cursor.execute(query)
